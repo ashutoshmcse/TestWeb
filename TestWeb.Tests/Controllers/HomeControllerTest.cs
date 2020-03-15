ccde2a90b478cb8a9de8714083b4ac7b037fc55c
Ashutosh Ranjan <ashutosh.mcse@gmail.com>
2020-03-15 18:26:07 +0530

    TestProject Added

---
+++ TestWeb.Tests/Controllers/HomeControllerTest.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+using TestWeb.Controllers;
+
+namespace TestWeb.Tests.Controllers
+{
+    [TestFixture]
+    public class HomeControllerTest
+    {
+        [Test]
+        public void IndexTest()
+        {
+            var controller = new HomeController();
+            var view = controller.Index();
+            Assert.NotNull(view);
+        }
+    }
+}
