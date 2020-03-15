0e8c138e823bec3ffaccd82b0b5d834384888da2
Ashutosh Ranjan <ashutosh.mcse@gmail.com>
2020-03-16 00:51:53 +0530

    another update as per resharper suggestion

--- TestWeb/Controllers/ValuesController.cs
+++ TestWeb/Controllers/ValuesController.cs
@@ -9,11 +9,6 @@ namespace TestWeb.Controllers
 {
     public class ValuesController : ApiController
     {
-        // GET api/values
-        public static IEnumerable<string> Get()
-        {
-            return new string[] { "value1", "value2" };
-        }
 
         // GET api/values/5
         public string Get(int id)
